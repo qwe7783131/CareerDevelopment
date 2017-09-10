5815e3972f161582954b760acb31dba608296da5
guanhong <844967548@qq.com>
2017-09-10 15:11:03 +0800

    guan
    ssm这下才完成

--- careerdevelopment/src/main/java/com/bugmaker/controller/StudentController.java
+++ careerdevelopment/src/main/java/com/bugmaker/controller/StudentController.java
@@ -1,5 +1,29 @@
 package com.bugmaker.controller;
 
-public class StudentController {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bugmaker.bean.Student;
+import com.bugmaker.service.StudentService;
+
+@Controller
+public class StudentController {
+	
+	@Resource
+	public StudentService studentService;
+	
+	@RequestMapping("test.do")
+	public String testcontroller(ModelMap model) {
+		List<Student> selectAllStudent = studentService.selectAllStudent();
+		model.put("selectAllStudent", selectAllStudent);
+		return "index";
+	}
 }
