ebab4f3c7cd355ffb30b272453e7e54061fd3166
limicala <2579749249@qq.com>
2017-09-11 15:44:31 +0800

    bbb

--- careerdevelopment/src/main/java/com/bugmaker/controller/StudentController.java
+++ careerdevelopment/src/main/java/com/bugmaker/controller/StudentController.java
@@ -1,20 +1,23 @@
 package com.bugmaker.controller;
 
-<<<<<<< HEAD
-=======
-import java.util.List;
 
-import javax.annotation.Resource;
-
+import com.bugmaker.bean.Student;
+import com.bugmaker.service.impl.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bugmaker.bean.Student;
-import com.bugmaker.service.impl.StudentService;
+import javax.annotation.Resource;
+import java.util.List;
 
 @Controller
->>>>>>> 644f799738e0eddc32151c71deb4567354bf70e3
 public class StudentController {
-
+	@Resource
+	public StudentService studentService;
+	@RequestMapping("test.do")
+ 	public String testcontroller(ModelMap model) {
+		List<Student> selectAllStudent = studentService.selectAllStudent();
+		model.put("selectAllStudent", selectAllStudent);
+		return "index";
+	}
 }
