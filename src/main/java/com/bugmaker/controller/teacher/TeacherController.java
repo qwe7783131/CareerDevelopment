bd3cd9ed509ab2f058821b0f79021e40f090718d
limicala <2579749249@qq.com>
2017-09-14 22:24:37 +0800

    整合玮轩[@hinry1121](https://github.com/hinry1121)的教师页面
    包括：
    - 基本数据管理模块
    - 学生职业方向选择模块

--- src/main/java/com/bugmaker/controller/teacher/TeacherController.java
+++ src/main/java/com/bugmaker/controller/teacher/TeacherController.java
@@ -9,8 +9,76 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
+
+    /**
+     * 教师首页
+     * @return
+     */
     @RequestMapping("index.do")
     public String indexView(){
         return "teacher/index";
+    }
+
+    /**
+     * 查看学生信息的页面
+     * @return
+     */
+    @RequestMapping("selectStu.do")
+    public String selectStuView(){
+        return "teacher/selectStu";
+    }
+
+    /**
+     * 查看教师信息的页面
+     * @return
+     */
+    @RequestMapping("selectTea.do")
+    public String selectTeaView(){
+        return "teacher/selectTea";
+    }
+
+    /**
+     * 查看专业负责人信息的页面
+     * @return
+     */
+    @RequestMapping("selectProfessTea.do")
+    public String selectProfessTeaView(){
+        return "teacher/selectProfessTea";
+    }
+
+    /**
+     * 查看企业教师信息的页面
+     * @return
+     */
+    @RequestMapping("selectOut.do")
+    public String selectOutView(){
+        return "teacher/selectOut";
+    }
+
+    /**
+     * 发布专业方向的页面
+     * @return
+     */
+    @RequestMapping("releaseDirect.do")
+    public String releaseDirectView(){
+        return "teacher/releaseDirect";
+    }
+
+    /**
+     * 查看学生志愿填报的页面
+     * @return
+     */
+    @RequestMapping("selectVoluntaryReport.do")
+    public String selectVoluntaryReportView(){
+        return "teacher/selectVoluntaryReport";
+    }
+
+    /**
+     * 查看分班名单
+     * @return
+     */
+    @RequestMapping("selectClassDivide.do")
+    public String selectClassDivideView(){
+        return "teacher/selectClassDivide";
     }
 }
