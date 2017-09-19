511d6fe991fe554e57fc0e13f877f2593ef91eea
whyzdz1547 <994421975@qq.com>
2017-09-19 14:15:34 +0800

    ClassRoom太难听了，改为DirectionClass专业方向班级

---
+++ src/main/java/com/bugmaker/bean/DirectionClass.java
@@ -0,0 +1,46 @@
+package com.bugmaker.bean;
+
+/**
+ *    班级表
+ */
+public class DirectionClass {
+    private String id;
+
+    private String name;
+
+    private String deptId;
+
+    private String direcId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(String deptId) {
+        this.deptId = deptId == null ? null : deptId.trim();
+    }
+
+    public String getDirecId() {
+        return direcId;
+    }
+
+    public void setDirecId(String direcId) {
+        this.direcId = direcId == null ? null : direcId.trim();
+    }
+}
