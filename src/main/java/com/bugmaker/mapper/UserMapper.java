a237ca007b08acb859ed6318ba5d4cd4224a0858
guanhong <844967548@qq.com>
2017-09-21 09:30:12 +0800

    查询，删除所有教师mapper,mapping

--- src/main/java/com/bugmaker/mapper/UserMapper.java
+++ src/main/java/com/bugmaker/mapper/UserMapper.java
@@ -7,11 +7,31 @@ import java.util.List;
  */
 public interface UserMapper {
     /**
-     * 登录用户查询用户
+     * 查询所有的教师
+     * @param type
+     * @return
+     */
+    List<User> getAllTeacher(int type);
+    /**
+     * 根据id修改用户信息
+     * @param user  传入需要修改的用户信息
+     * @return
+     */
+    int updateUserById(User user);
+    /**
+     * 根据id查询登录用户信息
      * @param userName
      * @return User 返回用户信息
      */
     User getUserByUserName(String userName);
+
+    /**
+     * 根据用户id删除对应的用户
+     * @param userId
+     * @return
+     */
+    int deleteUserById(String userId);
+
     /**
      * 插入用户基表数据
      * @param user
