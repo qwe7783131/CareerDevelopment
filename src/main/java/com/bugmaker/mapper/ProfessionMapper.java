8ac84673bfe2418836aac2cb4d86247ab98ac85d
whyzdz1547 <994421975@qq.com>
2017-09-23 14:23:09 +0800

    professionmapper

--- src/main/java/com/bugmaker/mapper/ProfessionMapper.java
+++ src/main/java/com/bugmaker/mapper/ProfessionMapper.java
@@ -8,4 +8,19 @@ public interface ProfessionMapper {
 
 	//获得所有专业
 	List<Profession> getAllProfessions();
+	
+	//获取所有专业外加学院
+	List<Profession> getAllProfessionsAndDept();
+
+	//通过学院id获取专业列表
+	List<Profession> getProfessionsByDeptID(String deptId);
+
+	//添加专业
+	int insertProfession(Profession profession);
+
+	//修改专业名称
+	int modifyProfession(Profession profession);
+
+	//删除专业
+	int deleteProfession(String professId);
 }
