f82941e9d51a749fa54ba0faa9525e0ac02a8b71
whyzdz1547 <994421975@qq.com>
2017-09-24 21:44:37 +0800

    添加专业负责人和系统管理员类型常量

--- src/main/java/com/bugmaker/constant/UserConstant.java
+++ src/main/java/com/bugmaker/constant/UserConstant.java
@@ -5,9 +5,11 @@ public interface UserConstant {
 	/**
 	 * 用户类型常量
 	 */
-	public static int StudentType = 1;       //学生用户类型
-	public static int TeacherType = 2;       //教师用户类型
-	public static int OutTeacherType = 3;    //企业教师用户类型
-	public static int CounselorType = 4;     //辅导员用户类型
-	public static int LeaderType = 5;        //系领导用户类型
+	public static int StudentType = 1;                //学生用户类型
+	public static int TeacherType = 2;                //教师用户类型
+	public static int OutTeacherType = 3;             //企业教师用户类型
+	public static int CounselorType = 4;              //辅导员用户类型
+	public static int LeaderType = 5;                 //系领导用户类型
+	public static int ProfessionHeadType = 6;         //专业负责人
+	public static int AdminType = 0;                  //系统管理员
 }
